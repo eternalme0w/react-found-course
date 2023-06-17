5d929ed6da66d0dc0d2cb97d0cd817bf042b6025
eternalme0w <lenny.study@yandex.ru>
2023-06-17 14:35:21 +0300

    UI component MyButton created

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./styles/App.css";
 import Counter from "./components/Counter";
 import Post from "./components/Post";
 import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
 
 
 
@@ -23,6 +24,8 @@ function App() {
     return (
 
         <div className="App">
+
+            <MyButton disabled>delete</MyButton>
 
             <PostList posts={posts} title="JS posts"/>
 
