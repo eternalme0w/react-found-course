390f04219f69cee3c1b2e250a0ad2677c49c34de
eternalme0w <lenny.study@yandex.ru>
2023-06-17 11:16:17 +0300

    posts are storing in an array and includes in App by map

--- src/App.js
+++ src/App.js
@@ -9,13 +9,24 @@ import Post from "./components/Post";
 
 function App() {
 
+    const [posts, setPosts] = useState([
 
+        {id: 1, title: "JavaScript", body: "Description"},
+        {id: 2, title: "JavaScript", body: "Description"},
+        {id: 3, title: "JavaScript", body: "Description"},
+        {id: 4, title: "JavaScript", body: "Description"},
+        {id: 5, title: "JavaScript", body: "Description"},
+
+    ])
 
     return (
 
         <div className="App">
-        
-            <Post post={{id: 1, title: "JavaScript", body: "Description"}}/>
+            
+            {posts.map(post => 
+                
+                <Post post={post}/>
+            )}
         
         </div>
     
