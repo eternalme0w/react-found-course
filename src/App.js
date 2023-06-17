ff34e4709ab0c3b95addce465602e402325b41cd
eternalme0w <lenny.study@yandex.ru>
2023-06-17 09:09:01 +0300

    linked input and string

--- src/App.js
+++ src/App.js
@@ -4,11 +4,21 @@ import React, {useState} from "react";
 
 function App() {
 
+  const [str, setStr] = useState('zxc');   
 
   return (
 
     <div className="App">
       
+      <h1>{str}</h1>
+
+      <input 
+
+        type="text"
+        value={str}
+        onChange={event => setStr(event.target.value)}
+        
+      />
 
     </div>
     
