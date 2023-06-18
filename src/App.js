2c46bfd536433496c8597dfa42be879037bd6a1b
eternalme0w <lenny.study@yandex.ru>
2023-06-18 23:22:37 +0300

    usePosts hook complited

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React, {useMemo, useState} from "react";
+import React, {useState} from "react";
 import "./styles/App.css";
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
 import PostFilter from "./components/PostFilter";
 import MyModal from "./components/UI/modal/MyModal";
 import MyButton from "./components/UI/button/MyButton";
+import { usePosts} from "./hooks/usePosts";
 
 
 function App() {
@@ -23,22 +24,8 @@ function App() {
     const [modal, setModal] = useState(false);
     
 
-    const sortedPosts = useMemo(() => {
 
-        if (filter.sort) {
-
-            return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]));
-        }
-
-        return posts;
-
-    }, [filter.sort, posts]);
-
-    const sortedAndSearchedPosts = useMemo(() => {
-
-        return sortedPosts.filter(post => post.title.includes(filter.query));
-
-    }, [filter.query, sortedPosts]);
+    const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
     
     const createPost = newPost => {
 
